5d1cebc4e0657ff1f1a7a38fe0be94471f4a9aa5
Maurice Marvin <maurice.marvin8812@gmail.com>
2023-08-07 20:03:48 +0700

    Initial Commit - Add parent pom.xml - Add gitignore

---
+++ src/main/java/com/maurice/va/VirtualAccountApplication.java
@@ -0,0 +1,13 @@
+package com.maurice.va;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualAccountApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VirtualAccountApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/maurice/va/VirtualAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurice.va;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualAccountApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
