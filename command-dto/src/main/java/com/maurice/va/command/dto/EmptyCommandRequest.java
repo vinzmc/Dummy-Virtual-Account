9418d1cb715c564a0b4ce9d8147371bce2fa8342
Maurice Marvin <maurice.marvin8812@gmail.com>
2023-08-10 19:32:06 +0700

    replace getInstance with singleton

--- command-dto/src/main/java/com/maurice/va/command/dto/EmptyCommandRequest.java
+++ command-dto/src/main/java/com/maurice/va/command/dto/EmptyCommandRequest.java
@@ -1,6 +1,8 @@
 package com.maurice.va.command.dto;
 
 public final class EmptyCommandRequest {
+  private static EmptyCommandRequest INSTANCE;
+
   // Private constructor to prevent instantiation
   private EmptyCommandRequest() {
     // Private constructor to prevent instantiation
@@ -8,6 +10,10 @@ public final class EmptyCommandRequest {
 
   // Factory method to obtain an instance
   public static EmptyCommandRequest getInstance() {
-    return new EmptyCommandRequest();
+    if(INSTANCE == null) {
+      INSTANCE = new EmptyCommandRequest();
+    }
+
+    return INSTANCE;
   }
 }
