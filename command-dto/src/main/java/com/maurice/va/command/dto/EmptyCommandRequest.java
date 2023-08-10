15ad866d2e52643b72f8bf0936d7abe959701ab0
Maurice Marvin <maurice.marvin8812@gmail.com>
2023-08-10 19:29:00 +0700

    add private constructor

--- command-dto/src/main/java/com/maurice/va/command/dto/EmptyCommandRequest.java
+++ command-dto/src/main/java/com/maurice/va/command/dto/EmptyCommandRequest.java
@@ -1,6 +1,12 @@
 package com.maurice.va.command.dto;
 
-public class EmptyCommandRequest {
+public final class EmptyCommandRequest {
+  // Private constructor to prevent instantiation
+  private EmptyCommandRequest() {
+    // Private constructor to prevent instantiation
+  }
+
+  // Factory method to obtain an instance
   public static EmptyCommandRequest getInstance() {
     return new EmptyCommandRequest();
   }
